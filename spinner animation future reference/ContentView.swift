9b60c73a76ac8719a992262f3d39ce4adb781b9c
Vincent Cloutier <vincentxcloutier@gmail.com>
2020-11-09 09:58:12 -0500

    Initial Commit

---
+++ spinner animation future reference/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spinner animation future reference
+//
+//  Created by Cloutier, Vincent on 2020-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spinner animation future reference/spinner_animation_future_referenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  spinner_animation_future_referenceApp.swift
+//  spinner animation future reference
+//
+//  Created by Cloutier, Vincent on 2020-11-09.
+//
+
+import SwiftUI
+
+@main
+struct spinner_animation_future_referenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
